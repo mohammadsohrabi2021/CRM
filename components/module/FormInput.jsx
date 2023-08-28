a03865fb371ed87b2535eb79f40a3bd15eae24a4
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2023-08-28 23:51:18 +0330

    create component formInput  done

---
+++ components/module/FormInput.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function FormInput({ name, label, type, value,onChange }) {
+  return (
+    <div className="form-input">
+      <label htmlFor={name}>{label}</label>
+      <input
+        type={type}
+        id={name}
+        name={name}
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
+}
+
+export default FormInput;
