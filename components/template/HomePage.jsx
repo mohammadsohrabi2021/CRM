dfdba01571e47710e1076143267dccb4dad52fb0
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2023-08-28 23:53:09 +0330

    create component HomePage done

---
+++ components/template/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Card from '../module/Card';
+
+function HomePage({ customers }) {
+    return (
+      <div>
+        {customers.map((customer) => (
+          <Card key={customer._id} customer={customer} />
+        ))}
+      </div>
+    );
+  }
+  
+  export default HomePage;
+
